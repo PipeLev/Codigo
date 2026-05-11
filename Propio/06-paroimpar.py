565d2201ad6df3a322d7859f4a0315b810bf80eb
PipeLev <felipelevican02@gmail.com>
2026-05-11 10:16:54 -0400

    new + comment

--- Propio/06-paroimpar.py
+++ Propio/06-paroimpar.py
@@ -1,6 +1,6 @@
 num1 = int(input("Ingrese su numero para saber si es par o impar:"))
 
-if num1 % 2 == 0:
+if num1 % 2 == 0: #resto de la división
     print(f"Es par")
 else:
     print(f"Es impar")
