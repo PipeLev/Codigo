4dd936067c167dfc77ef593ecb9bf197184b2614
PipeLev <felipelevican02@gmail.com>
2026-05-11 12:32:06 -0400

    new

--- Clases de Python/02-numeros.py
+++ Clases de Python/02-numeros.py
@@ -1,4 +1,11 @@
-#Int
-a = 2
-#float
-b = 2.2
+numero_complejo = 4 + 2j
+num = complex(4,2)
+
+print(f"{num} {numero_complejo}")
+
+#limitar
+pi = 3.1416
+print(f"el valor de pi es de {pi:.2f} ")
+
+#redondeo
+print(f"50.99 y {round(50.99)}")
