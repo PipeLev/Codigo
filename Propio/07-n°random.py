01ff03ee237e7890015f8a8aaaddc6bb4d057fdc
PipeLev <felipelevican02@gmail.com>
2026-05-17 15:31:11 -0400

    Update

--- Propio/07-n°random.py
+++ Propio/07-n°random.py
@@ -5,7 +5,9 @@ print(f"Numero elegido {n}")
 if n < 10:
     print(f"el numero {n} es menor de 10")
 elif n > 50:
-    print(f"el numero {n} es mayor a 50")
+    print(f"el numero {n} es mayor de 50 y menor de 100")
+elif n > 100:
+    print(f"el numero {n} es mayor de 100")
 elif n == 120:
     print (f"{n} es el numero maximo")
 elif n == 0:
