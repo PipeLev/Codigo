de468048bfa9eaf21ba3f0b7e6f23158e6b6944a
PipeLev <felipelevican02@gmail.com>
2026-05-17 15:39:29 -0400

    Update

--- Propio/07-n°random.py
+++ Propio/07-n°random.py
@@ -1,14 +1,15 @@
 import random
+
 n = random.randrange(120)
 print(f"Numero elegido {n}")
 
-if n < 10:
-    print(f"el numero {n} es menor de 10")
-elif n > 50:
-    print(f"el numero {n} es mayor de 50 y menor de 100")
-elif n > 100:
-    print(f"el numero {n} es mayor de 100")
+if n == 0:
+    print (f"no hay nada, no existe")
 elif n == 120:
     print (f"{n} es el numero maximo")
-elif n == 0:
-    print (f"no hay nada, no existe")
+elif n < 10:
+    print(f"el numero {n} es menor de 10")
+elif n > 100:
+    print(f"el numero {n} es mayor que 100")
+elif n > 50:
+    print(f"el numero {n} es mayor de 50 y menor de 100")
