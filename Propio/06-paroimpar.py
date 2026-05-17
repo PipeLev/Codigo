cf2abd4559706203cdb80de48dfb325dd4f3d4ea
PipeLev <felipelevican02@gmail.com>
2026-05-17 16:00:32 -0400

    Update

--- Propio/06-paroimpar.py
+++ Propio/06-paroimpar.py
@@ -1,6 +1,19 @@
-num1 = int(input("Ingrese su numero para saber si es par o impar:"))
+# Asumimos que 'seleccion' se define con 1 inicialmente para entrar al flujo
+seleccion = 1
 
-if num1 % 2 == 0: #resto de la división
-    print(f"Es par")
-else:
-    print(f"Es impar")
+while seleccion == 1:
+    num1 = int(input("Ingrese su numero para saber si es par o impar: "))
+
+    if num1 % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")
+    
+    # Preguntamos dentro del bucle si quiere continuar
+    print("\n¿Deseas saber otro numero?")
+    print("1. Si")
+    print("2. No")
+    seleccion = int(input("Seleccione una opción: "))
+
+if seleccion == 2:
+    print("Cerrando...")
