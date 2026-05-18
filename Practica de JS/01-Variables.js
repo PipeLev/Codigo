778e78a7508a2d60419f8aaf3b49e9d7a19c8407
PipeLev <felipelevican02@gmail.com>
2026-05-18 18:29:07 -0400

    JS

---
+++ Practica de JS/01-Variables.js
@@ -0,0 +1,31 @@
+let anime = "Konosuba"; //variable de palabras
+
+console.log (anime);
+
+//numeros
+let entero = 100;
+let real = 3.14;
+
+console.log (entero);
+
+//boleanos
+let verdadero = true;
+let seleccions = false;
+
+console.log (verdadero);
+//lista
+
+let mascota = ["gato", "perro"];
+let numeros = [91, 28, 39];
+
+console.log (mascota);
+console.log (numeros);
+
+//Objeto
+
+let jugador = {
+    9: "Zampedri",
+    1: "Toselli"
+};
+
+console.log (jugador);
