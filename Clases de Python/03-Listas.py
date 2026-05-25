5551a43769278019c923ce185cc15f4cae4f3312
PipeLev <felipelevican02@gmail.com>
2026-05-25 12:32:05 -0400

    Update

--- Clases de Python/03-Listas.py
+++ Clases de Python/03-Listas.py
@@ -1,14 +1,59 @@
 #Listas
 
 #Primera forma de Declaracion de Lista Mixta
-lista1=["felipe",2,True,4,"felipe"]
+lista1=["felipe",18,True,4,"felipe"]
+ramos = []#nada
 
 #Segunda forma de Declaracion de Lista Numerica
-lista2=list([1,2,3,4,5])
+n=list([1,4,3,6,5,0,8])
 
 #Metodo para las listas
 #01- COUNT()- cuenta el numero de veces que se repite un elemento en la lista
 print(lista1.count("felipe"))
 
-print(lista1[2])
+print(ramos)
 
+#agregar un elemento al final de la lista
+ramos.append("quimica")
+print(ramos)
+
+ramos.append("programacion")
+print(ramos)
+
+ramos.append("introduccion a la matematicas")
+print(ramos)
+
+#modificar el nombre de un elemento de una lista
+ramos[1] = "progamacion basica"
+
+print(ramos)
+
+#otra forma
+ramos.insert(1, "habilidades comunicativas")
+
+print(ramos)
+
+#eliminar elemento de la lista
+ramos.pop(3)
+
+print(ramos)
+
+#ordenar la lista
+ramos.sort()
+print(ramos)
+
+n.sort()
+print(n)
+
+#ordenar cor caracteres
+
+ramos.sort(key=len)
+print(ramos)
+
+#extension
+ramos_semestre = ["intro a la fisica", "algebra", "ciudadania"]
+print(ramos_semestre)
+
+ramos.extend(ramos_semestre)
+
+print(ramos)
