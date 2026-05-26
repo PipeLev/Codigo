4761143d8293786b2b301b69615a4528c172703d
PipeLev <felipelevican02@gmail.com>
2026-05-25 20:36:19 -0400

    JS

--- Practica de JS/03-Usuario.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt("Ingresa tu nombre:");
-let password = prompt("Ingresa tu contraseña:");
-
-
-if (nombre === "FelipeLev" && password === "64213") {
-    console.log("Inicio de sesión correcto");
-} else {
-    console.log("Nombre o contraseña incorrectos");
-}
