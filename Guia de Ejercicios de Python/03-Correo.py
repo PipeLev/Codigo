5e7d08d3a40fed84e0fcb23252c96aecf51b37fe
PipeLev <felipelevican02@gmail.com>
2026-05-31 19:31:16 -0400

    new

---
+++ Guia de Ejercicios de Python/03-Correo.py
@@ -0,0 +1,9 @@
+nombre = input("Ingresa tu nombre y apellido para generate el correo institucional: ")
+
+nombre= nombre.lower()
+
+nombre = nombre.strip()
+
+nombre = nombre.replace(" ", ".")
+
+print(f"Tu correo institucional es: {nombre}@alumnos.ulagos.cl")
