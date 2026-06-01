ce9c4a4d5f84456831e2254a102dae71ce7412fe
PipeLev <felipelevican02@gmail.com>
2026-05-31 21:43:40 -0400

    update

--- Guia de Ejercicios de Python/02-Rut.py
+++ Guia de Ejercicios de Python/02-Rut.py
@@ -1,10 +1,10 @@
-rut = str(" 19.543.872-K ")
+rut = " 19.543.872-K "
 
 print(f"Este sistema es para tener en limpio un rut como por ejemplo: {rut}")
 
 rut = rut.strip()
 
-rut= rut.replace(".", "")
+rut = rut.replace(".", "")
 
 print(f"La longitud del RUT es de: {len(rut)} Caracteres")
-print(f"El rut en Limpio es asi: {rut}")
+print(f"El RUT en Limpio es asi: {rut}")
