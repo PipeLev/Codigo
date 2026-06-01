b935bd8dfd64790c380ac83019478b7b5e77a7b5
PipeLev <felipelevican02@gmail.com>
2026-05-31 20:58:19 -0400

    new

---
+++ Guia de Ejercicios de Python/04-Sensor.py
@@ -0,0 +1,13 @@
+sensor = 1024.7689
+
+sensor_entero = int(sensor)
+
+sensor_redondeo = round(sensor, 2)
+
+print(f"El valor original del sensor es de: {sensor}")
+
+print(f"El valor como numero entero del sensor es de: {sensor_entero}")
+
+print(f"El valor del sensor con dos decimales es de: {sensor_redondeo}")
+
+print(f"El valor redondeado del sensor es de: {round(sensor)}")
